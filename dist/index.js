8a13861eb7c5701a42de2f472e0d7040ba5b5a43
kjirou <kjirou.web@gmail.com>
2016-08-12 02:18:24 +0900

    Build

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,14 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var objectFromKeys = exports.objectFromKeys = function objectFromKeys(keys) {
+  var dict = {};
+  keys.forEach(function (key) {
+    return dict[key] = key;
+  });
+  return dict;
+};
+
 var within = exports.within = function within(num, minNum, maxNum) {
   return Math.min(Math.max(num, minNum), maxNum);
 };
